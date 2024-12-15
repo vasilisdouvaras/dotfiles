204db0ed0c59a23869bad08f62768a4c52a03b02
Vasilis Douvaras <vasilisdouvaras@gmail.com>
2024-12-15 16:53:22 +0100

    chore: fix formatting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-		ensure_installed = { "help", "javascript", "typescript", "lua" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		}
-	}
+        ensure_installed = { "help", "javascript", "typescript", "lua" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        }
+    }
 }
-
