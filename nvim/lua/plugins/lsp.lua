a34c2184c56fa1366a8d3c2c1d435763357b1ebe
Vasilis Douvaras <vasilis.douvaras@advinans.se>
2024-12-15 18:38:48 +0100

    feat: add binidng for autocomplete

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 }),
                 snippet = {
                     expand = function(args)
