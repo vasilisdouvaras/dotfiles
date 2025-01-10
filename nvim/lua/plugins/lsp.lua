c01f7d3667e27724b6c9207f15cc591bb0307a88
Vasilis Douvaras <vasilis.douvaras@advinans.se>
2025-01-10 08:26:55 +0100

    feat: add go to definition split

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -70,7 +70,7 @@ return {
 
           vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
           vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>:vsplit | lua vim.lsp.buf.definition()<cr>', opts)
           vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
           vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
           vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
@@ -84,8 +84,10 @@ return {
       -- Customizing LSP hover windows
       vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
         border = "rounded", -- You can use 'single', 'double', 'rounded', or 'none'
-        focusable = false, -- If set to true, the window can be focused
+        focusable = false,  -- If set to true, the window can be focused
       })
+
+      vim.api.nvim_create_augroup('LspAutoFormat', { clear = true })
 
       require('mason-lspconfig').setup({
         ensure_installed = {},
@@ -93,7 +95,21 @@ return {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            require('lspconfig')[server_name].setup({
+              on_attach = function(client, bufnr)
+                -- Enable auto format on save
+                if client.server_capabilities.documentFormattingProvider then
+                  vim.api.nvim_clear_autocmds({ group = 'LspAutoFormat' })
+                  vim.api.nvim_create_autocmd('BufWritePre', {
+                    group = 'LspAutoFormat',
+                    buffer = bufnr,
+                    callback = function()
+                      vim.lsp.buf.format()
+                    end,
+                  })
+                end
+              end,
+            })
           end,
           require('lspconfig').ts_ls.setup {
             on_attach = function(client)
